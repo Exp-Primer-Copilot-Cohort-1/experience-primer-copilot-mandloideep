75371bc66931fa91b9da4cf82aa3eee9afe8a3ff
mandloideep <140644454+mandloideep@users.noreply.github.com>
2024-01-04 05:42:26 +0000

    copilot updated commit

--- skills.js
+++ skills.js
@@ -1,5 +1,3 @@
 function calculateNumber(var1, var2) {
-  const roundedVar1 = Math.round(var1);
-  const roundedVar2 = Math.round(var2);
-  return roundedVar1 + roundedVar2;
+  console.log(var1 + var2);
 }
